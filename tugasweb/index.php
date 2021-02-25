dbcc84a4401d4e42a367ba6ac8ddcd4d331b0289
Ayuchandra <79451566+Ayuchandra@users.noreply.github.com>
2021-02-25 23:29:43 +0800

    Add files via upload

---
+++ tugasweb/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Selamat Datang di Website Chandra</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Menghasilkan lulusan yang berkualitas dan berdaya saing tinggi dalam bidang TIK seperti programer, administrator basis data, teknisi jaringan, multimedia.">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+    <link rel="canonical" href="http://informatika.undiksha.ac.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="">
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body>
+    <div class="jumbotron text-center">
+        <h1>Sistem Informasi Tari Bali</h1>
+        <p>Expressions of the soul that contain elements of beauty</p>
+        <img src="media\tari-bali.jpg">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <center><h3>Deskripsi</h3</center>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>Kesenian</h3</center>
+            </div>
+            <div class="col-sm-4">
+                <center><h3>Bali</h3</center>
+        
+            </div>
+        </div>
+    </div>
+    <div><p align="center"> Tari pendet merupakan salah satu tarian selamat datang yang paling tua di Pulau Bali.
+                Jenis tarian penyambutan ini dibawakan oleh sekelompok remaja putri yang masing-masing membawa <p align="center">mangkok perak yang berisi bunga warna-warni
+                para penari menaburkan bunga-bunga yang mereka bawa ke arah penonton atau tamu yang disambut, sebagai ucapan selamat datang.
+                <p align="center">Penggagas tarian ini yaitu seniman kelahiran desa Sumertha, Denpasar, yaitu I Wayan Rindi dan Ni Ketut Reneng. Keduanya menciptakan tari Pendet penyambutan dengan empat orang penari</p></div>
+</body>
+
+</html>
